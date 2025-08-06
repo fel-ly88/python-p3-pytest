8313cbb9308c490d32f67f0895043fa2a9b4b4bb
Your Name <your-email@example.com>
2025-08-06 20:46:08 +0300

    commit changes

--- lib/testing/test_not_none.py
+++ lib/testing/test_not_none.py
@@ -5,3 +5,10 @@ from not_none_functions import return_not_none
 # def test_return_not_none():
 #     '''in not_none_functions, function "return_not_none" returns a value that is not None.'''
 #     assert False
+
+def test_function_returns_not_none():
+    '''Your function should return a value that is not None.'''
+    assert your_function_name() is not None
+    
+def your_function_name():
+    return "something"
